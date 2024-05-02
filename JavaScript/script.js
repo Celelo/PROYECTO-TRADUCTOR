1799fd24bb96e073db6f42ca0aac01be2085c9de
Célelo <mariaceleste.lopez41@gmail.com>
2024-05-02 20:42:10 -0300

    finished url

---
+++ JavaScript/script.js
@@ -0,0 +1,97 @@
+// const $ = (element) => document.querySelector(element);
+// let initTextSelect;
+// let TranslateTextSelect;
+// const url = 'https://text-translator2.p.rapidapi.com/translate'; // Definir url como variable global
+
+// async function fetchData() {
+//   const languagesUrl = 'https://text-translator2.p.rapidapi.com/getLanguages';
+
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': '9ed367b64amsh301c82186b38cccp158a21jsn82a5a9a0bbc9',
+//       'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+//     }
+//   };
+
+//   try {
+//     const response = await fetch(languagesUrl, options);
+//     const data = await response.json();
+//     let languages = data.data.languages;
+
+//     languages.forEach(element => {
+//       $('#textSelect').innerHTML += `<option value="${element.code}">${element.name}</option>`;
+//       $('#translateSelect').innerHTML += `<option value="${element.code}">${element.name}</option>`;
+//     });
+
+//     $('#textSelect').addEventListener('click', () => {
+//       console.log($('#textSelect').value);
+//       initTextSelect = $('#textSelect').value;
+//     });
+
+//     $('#translateSelect').addEventListener('click', () => {
+//       console.log($('#translateSelect').value);
+//       TranslateTextSelect = $('#translateSelect').value;
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// fetchData(); 
+
+// $('#btn').addEventListener('click', async () => {
+//   let textTranslate = $('#textSelect').value;
+
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/x-www-form-urlencoded',
+//       'X-RapidAPI-Key': '9ed367b64amsh301c82186b38cccp158a21jsn82a5a9a0bbc9',
+//       'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+//     },
+//     body: new URLSearchParams({
+//       source_language: initTextSelect,
+//       target_language: TranslateTextSelect,
+//       text: textTranslate
+//     })
+//   };
+
+//   try {
+//     const response = await fetch(url, options); // Utilizar url definida globalmente
+//     const result = await response.text();
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+
+
+
+const $ = (element) => document.querySelector(element);
+let initTextSelect;
+let TranslateTextSelect;
+const url = 'https://text-translator2.p.rapidapi.com/translate';
+
+async function fetchData() {
+  const languagesUrl = 'https://text-translator2.p.rapidapi.com/getLanguages';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '9ed367b64amsh301c82186b38cccp158a21jsn82a5a9a0bbc9',
+      'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await fetch(languagesUrl, options);
+    const data = await response.json();
+    let languages = data.data.languages;
+    addOptionsToSelects(languages);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
